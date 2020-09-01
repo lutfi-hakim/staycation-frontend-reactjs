193b562cd705c3f9efb4caf74ee967d9b010a71f
lutfi-hakim <21lutfihakim@gmail.com>
2020-09-01 19:09:27 +0700

    misstype + complete component parts footer

---
+++ src/parts/Footer.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+import IconText from 'parts/IconText'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-auto" style={{ width: 350 }}>
+                        <IconText />
+                        <p className="brand-tagline">
+                            We kaboom your beauty holiday instantly and memorable
+                        </p>
+                    </div>
+                    <div className="col-auto mr-5">
+                        <h6 className="mt-2">For Beinners</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/register">New Account</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <Button type="link" href="/properties">Start Book a Room</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <Button type="link" href="/use-payments">Use Payments</Button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-2 mr-5">
+                        <h6 className="mt-2">Explore Us</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/careers">Our Carrers</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <Button type="link" href="/privacy">Privacy</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <Button type="link" href="/terms">Terms & Condisitions</Button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <h6 className="mt-2">Connect Us</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button isExternal type="link" href="mailto:support@staycation.ic">support@staycation.id</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <Button isExternal type="link" href="tel:+622134455676">02134455676</Button>
+                            </li>
+                            <li className="list-group-item">
+                                <span>Staycation, Jakarta</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col text-center copyrights">
+                        Copyright 2019 - All right reserved - Staycation
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
